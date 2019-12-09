388079af140474e524a63f68298214b753addffb
Arwaalissa <Arwaalissa@hotmail.com>
2019-12-09 20:39:05 +0300

    Create key.js

---
+++ assets/js/key.js
@@ -0,0 +1,23 @@
+
+  // Your web app's Firebase configuration
+  var config = {
+    apiKey: "AIzaSyDY8DVa3u_zatu1XBlUHFIMtRy31jbPbyo",
+    authDomain: "my-sticker-578af.firebaseapp.com",
+    databaseURL: "https://my-sticker-578af.firebaseio.com",
+    storageBucket: "my-sticker-578af.appspot.com",
+  };
+  firebase.initializeApp(config);
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyDY8DVa3u_zatu1XBlUHFIMtRy31jbPbyo",
+//     authDomain: "my-sticker-578af.firebaseapp.com",
+//     databaseURL: "https://my-sticker-578af.firebaseio.com",
+//     projectId: "my-sticker-578af",
+//     storageBucket: "my-sticker-578af.appspot.com",
+//     messagingSenderId: "487261244031",
+//     appId: "1:487261244031:web:9f542291a603422986cd4a"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+
+// api Key for my GIPHY
+var apiKeyGif='hWj37UcM2vSFo5kki51Q86yDTGKYJ5WI';
